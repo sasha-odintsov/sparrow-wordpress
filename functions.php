5cf022a1752cd179327112148ea5987777122181
Sasha <aleks-one@ukr.net>
2022-10-15 15:09:01 +0300

    Added sidebar and page and category files

--- functions.php
+++ functions.php
@@ -3,6 +3,19 @@
 add_action('wp_enqueue_scripts', 'style_theme');
 add_action('wp_footer', 'scripts_theme');
 add_action('after_setup_theme', 'theme_register_nav_menu');
+add_action( 'widgets_init', 'register_my_widgets' );
+
+function register_my_widgets(){
+  register_sidebar( array(
+    'name'          => 'Left Sidebar',
+    'id'            => 'left-sidebar',
+    'description'   => 'Left Sidebar',
+    'before_widget' => '<div class="widget %2$s">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h5 class="widget-title">',
+    'after_title'   => '</h5>'
+  ) );
+}
 
 function theme_register_nav_menu() {
     register_nav_menu('header-menu', 'Header Menu');
